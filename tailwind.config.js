08d8a737882c301a1322934c5efa6d8759af0414
dotclang <edyridwanh@gmail.com>
2025-08-24 21:53:25 +0700

    fix: asset v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'selector',
-  content: [
-    './resources/views/**/*.blade.php',
-    './resources/js/**/*.js',
-    './resources/css/**/*.css',
-    './src/**/*.php',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    darkMode: 'selector',
+    // ...
 };
